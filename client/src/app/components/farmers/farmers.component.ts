606dc28598803a4b8d0165603686ca4e6d8e3bd1
Tijesunimi Peters <tijesunimi48@gmail.com>
2020-08-09 03:06:52 -0500

    Done with preliminary layout

---
+++ client/src/app/components/farmers/farmers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-farmers',
+  templateUrl: './farmers.component.html',
+  styleUrls: ['./farmers.component.scss']
+})
+export class FarmersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
