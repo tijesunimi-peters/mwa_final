9600efacdf230aab91619e68f49161b7c5be452f
Rina Tamrakar <rina@Rinas-MacBook-Pro.local>
2020-08-08 19:02:58 -0500

    Updated the schema

--- server/models/product.js
+++ server/models/product.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose');
 var ProductSchema = new mongoose.Schema({
-    name:{
-       type: String,
-        required: true},
-    category:String,
-    description:String,
+    name: {
+        type: String,
+        required: true
+    },
+    category: String,
+    description: String,
     price: {
         type: Number,
         required: true
     },
-    farmer:{name:String, email: String}
-    
+    farmer: {
+        _id: mongoose.Types.ObjectId,
+        name: String,
+        email: String
+    }
+
 });
 module.exports = mongoose.model("Product", ProductSchema);
