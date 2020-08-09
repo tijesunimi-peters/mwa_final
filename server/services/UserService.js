8cf98bfe635585e730e064ceeae348910d0a8ad8
Ataklete <65136939+Ataklete@users.noreply.github.com>
2020-08-09 13:59:07 -0500

    Minor update

--- server/services/UserService.js
+++ server/services/UserService.js
@@ -1,24 +1,9 @@
 
 const { User } = require("../models");
 const jwt = require('jsonwebtoken');
-const { model } = require('mongoose')
-
-
-
 
 class UserService {
   static save(req, _, next) {
-    let userData = req.body
-    let user = new User(userData)
-    user.save((err, registeredUser) => {
-      if (err) {
-        console.log(err)
-      } else {
-        let payload = { subject: registeredUser._id }
-        let token = jwt.sign(payload,'dg344')
-        req.status(200).send({ token })
-      }
-    })
     next();
   }
 
@@ -38,22 +23,22 @@ class UserService {
   static login(req, _, next) {
     let userData = req.body
 
-   
-   User.findOne({ email: userData.email }).then((result)=>{
-      result.comparePassword(userData.password, (err, result) => {
-            if (result) {
-              req.success = {
-                data: "logged in"
-              }
-              next();
-            }
-              else {
-                next(err)
-              }
-    });
-  
 
-  })
+    User.findOne({ email: userData.email }).then((result) => {
+      result.comparePassword(userData.password, (err, result) => {
+        if (result) {
+          req.success = {
+            data: "logged in"
+          }
+          next();
+        }
+        else {
+          next(err)
+        }
+      });
+
+
+    })
 
 
   }
