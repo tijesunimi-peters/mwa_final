072fb1f82ad6fb77d6f32d8ab58e29aeb4f07a37
Ataklete <65136939+Ataklete@users.noreply.github.com>
2020-08-09 15:14:05 -0500

    Clean up

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,16 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const mongoose = require('mongoose');
-const User = require('../services/UserService')
-
 
 /* GET users listing. */
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
-// router.post('/sign', User.save)
-
 
 module.exports = router;
