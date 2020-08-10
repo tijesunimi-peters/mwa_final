17c9aeb28470056bf58eb3e82013d6921c1511ab
Ataklete <65136939+Ataklete@users.noreply.github.com>
2020-08-09 22:43:13 -0500

    Updating GetOrderforFarms In OrderServices

--- server/routes/api/v1/order.js
+++ server/routes/api/v1/order.js
@@ -4,5 +4,7 @@ const { OrderService } = require("../../../services");
 
 router.post("/orders", OrderService.createOrder);
 router.get("/orders/:customerId", OrderService.getOrders);
+router.get("/orders/farmers/:farmerId", OrderService.getOrderforFarmers);
+
 
 module.exports = router;
