ff077d5c9bb0e5500eb8db5a9d733cc13198d2e8
Ruby <rubytamrakar077@gmail.com>
2020-08-11 00:06:55 -0500

    fix users

--- server/routes/api/v1/users.js
+++ server/routes/api/v1/users.js
@@ -5,3 +5,5 @@ const { UserService, JwtService,EmailService } = require("../../../services");
 const { route } = require("./order");
 
 router.post("/users", UserService.save, JwtService.generateToken, EmailService.sendMail);
+router.get("/users", UserService.all);
+module.exports = router;
