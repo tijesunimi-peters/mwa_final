2e93e68986def07559268391148abe517d795343
Rina Tamrakar <rina@Rinas-MacBook-Pro.local>
2020-08-11 11:37:31 -0500

    removed logs

--- server/services/OrderService.js
+++ server/services/OrderService.js
@@ -1,64 +1,86 @@
 var express = require("express");
-var { Types } = require("mongoose")
+var {
+    Types
+} = require("mongoose")
 
-const { Order } = require("../models");
+const {Order} = require("../models");
 
 class OrderService {
-  static createOrder(req, _, next) {
-    let newOrder = new Order(req.body);
-    newOrder.save(function (err, order) {
-      if (err) return next(err);
-      req.success = {
-        status: 200,
-        data: order
-      };
-      next();
-    });
-  }
+    static createOrder(req, _, next) {
+        let newOrder = new Order(req.body);
+        newOrder.save(function (err, order) {
+            if (err) 
+                return next(err);
+            
+            req.success = {
+                status: 200,
+                data: order
+            };
+            next();
+        });
+    }
 
-  static updateStatus(req, _, next) {
-    const { id } = req.body;
-    const { status } = req.body;
+    static updateStatus(req, _, next) {
+        const {id} = req.body;
+        const {status} = req.body;
 
-    Order.updateOne({ _id: id }, { $set: { status: status } })
-      .then(result => {
-        req.success = { data: "success" };
-        next();
-      })
-      .catch(err => {
-        next(err);
-      });
-  }
-  
-  static getOrders(req, _, next) {
-    var query = { "customer._id": req.params.customerId };
-    Order.find(query, function (err, found) {
-      if (err) return next(err);
+        Order.updateOne({
+            _id: id
+        }, {
+            $set: {
+                status: status
+            }
+        }).then(result => {
+            req.success = {
+                data: "success"
+            };
+            next();
+        }).catch(err => {
+            next(err);
+        });
+    }
 
-      req.success = {
-        status: 200,
-        data: found
-      };
-      next();
-    });
-  }
+    static getOrders(req, _, next) {
+        var query = {
+            "customer._id": req.params.customerId
+        };
+        Order.find(query, function (err, found) {
+            if (err) 
+                return next(err);
+            
 
-  static getOrderforFarmers(req, _, next) {
+            req.success = {
+                status: 200,
+                data: found
+            };
+            next();
+        });
+    }
 
-    var query = { products: { $elemMatch: { "farmer._id": new Types.ObjectId(req.params.farmerId)} } } 
-    Order.find(query, function (err, found) {
-      console.log(found);
-     
-      if (err) return next(err);
+    static getOrderforFarmers(req, _, next) {
 
-      req.success = {
-        status: 200,
-        data: found,
-      };
-      next();
-    });
+        var query = {
+            products: {
+                $elemMatch: {
+                    "farmer._id": new Types.ObjectId(req.params.farmerId)
+                }
+            }
+        }
+        Order.find(query, function (err, found) {
 
-  }
+
+            if (err) 
+                return next(err);
+            
+
+            req.success = {
+                status: 200,
+                data: found
+            };
+            next();
+        });
+
+    }
 }
 
 module.exports = OrderService;
